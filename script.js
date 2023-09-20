050f4cfd47ad36b6f09499a7d263c1e5f0814b8e
GulraizAijaz <115898710+GulraizAijaz@users.noreply.github.com>
2023-09-20 01:34:21 -0700

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const input = document.getElementById("input-task");
-const task = document.getElementById("task-div");
 const submitBtn = document.getElementById("submit");
 const form = document.getElementById("input-form");
-const taskDiv = document.getElementById("task-div");
+const tasksDiv = document.getElementById("task-div");
 const delAll= document.getElementById("del-all");
 
 
 
+(function addSavedTasks(){
+    const storedTasks = JSON.parse(localStorage.getItem("storedTasks"));
+    if(storedTasks){
+        storedTasks.forEach(task=>{
+            // console.log(task)
+            let newTask = `<div id="task-option" class="task-div center-div"> <button id="done">done</button><span id="span">
+                        ${task.text}</span>
+                         <button>remove</button></div>`;
+        tasksDiv.innerHTML += newTask;
+        })
+    }
+})();
 
+function setTasksToLocalStorage(value){
+    const taskObj = {
+        text:value,
+        checked:false,
+    }
+    const allTasks = JSON.parse(localStorage.getItem("storedTasks"));
+    if(allTasks){
+        allTasks.push(taskObj);
+        localStorage.setItem("storedTasks", JSON.stringify(allTasks))
+    }
+    else{
+        const allTasks = [];
+        allTasks.push(taskObj);
+        localStorage.setItem("storedTasks", JSON.stringify(allTasks))
+    }
+}
 
+  
 
 
 form.addEventListener("submit",function addTask(e){
@@ -20,28 +48,35 @@ form.addEventListener("submit",function addTask(e){
         let newTask = `<div id="task-option" class="task-div center-div"> <button id="done">done</button><span id="span">
                         ${input.value}</span>
                          <button>remove</button></div>`;
-        task.innerHTML += newTask;
+        tasksDiv.innerHTML += newTask;
+        setTasksToLocalStorage(input.value);
         input.value="";
+        delAll.style="display:block;"
+
+        
     }
 });
 
-taskDiv.addEventListener("click", function done(e){
+tasksDiv.addEventListener("click", function done(e){
     if(e.target.innerHTML === "done"){
     
        const targetedTask = e.target.nextElementSibling;
        targetedTask.classList.toggle("checked");
+       
     }
     else if(e.target.innerHTML === "remove"){
         const elementToRemove = e.target.parentElement;
         elementToRemove.remove();
+        
+
     }
 })
+
 delAll.onclick = function(){
     const taskOptions = document.querySelectorAll(".task-div");
-    // console.log(taskOptions)
     taskOptions.forEach(item=>{
-        item.remove()
-    })
+        item.remove();
+    });
+    localStorage.removeItem("storedTasks");
+    delAll.style="display:none;";   
 }
-
-
